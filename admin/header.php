b12e2f8b6185d3c7bb0d45fe6dcb8eb140d40b6e
ShkretaAlbina <63610194+ShkretaAlbina@users.noreply.github.com>
2020-05-18 00:01:21 +0200

    Add files via upload

---
+++ admin/header.php
@@ -0,0 +1,62 @@
+
+<?php 
+	session_start();
+	if(!isset($_SESSION['user'])){
+		header('Location: ../../login.php');
+	}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ 
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+    <title><?php echo $page_title; ?></title>
+ 
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+  
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+   
+<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="../libs/css/custom.css" />
+</head>
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Admin</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="<?php  echo $page_name == "product" ? "active": "" ?>"><a href="../product/product.php">Products</a></li>
+        <li class="<?php  echo $page_name == "category" ? "active": "" ?>"><a href="../category/category.php">Categories</a></li>
+        <li><a href="../logout.php">Logout</a></li>
+        <li><a href="#">Users</a></li>  
+    </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+    <div class="container">
+ 
+        <?php
+        echo "<div class='page-header'>
+                <h1>{$page_title}</h1>
+            </div>";
+        ?>
+
